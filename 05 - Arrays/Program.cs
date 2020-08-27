db47bac65f5853c86fbb65268fc286d998c95b9e
Ricardo Mendes <rmcs87@gmail.com>
2020-08-27 09:50:21 -0300

    Declaração e Inicialização de Arrays.

---
+++ 05 - Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[10];
+            int[] numbers = new int[] { 0, 5, 10, 15, 20, 25 };
+            Console.WriteLine($"Numero no indice 1 = {numbers[1]}");
+            numbers[1] = 99;
+            Console.WriteLine($"Novo numero no indice 1 = {numbers[1]}");
+            int tam = numbers.Length;
+            Console.WriteLine($"O tamanho do vetor é =: {tam}");
+            Console.WriteLine($"O tamanho do vetor Names é =: {names.Length}");
+            Console.WriteLine($"O tipo do vetor Number é =: {numbers.GetType().ToString()}");
+            Console.WriteLine($"O tipo do vetor names é =: {names.GetType().ToString()}");
+        }
+    }
+}

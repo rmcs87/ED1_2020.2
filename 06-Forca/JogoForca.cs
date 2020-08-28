782dffe88de57b4fa62d6a5a5f1de65d5a8daaf4
Ricardo Mendes <rmcs87@gmail.com>
2020-08-28 10:00:35 -0300

    Base da Classe JogoForca

---
+++ 06-Forca/JogoForca.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+public class JogoForca
+{
+    //Atributos
+    string palavraAlvo;
+    string palavraExibida; //_ _ A _ _
+    string[] listaPalavras;
+    ArrayList chutesCertos;
+    ArrayList chutesErrados;
+    int maxErros = 5;
+    //Metodos
+    public JogoForca()
+    {
+        listaPalavras = new string[] { "uvv", "casa", "borboleta" };
+        chutesCertos = new ArrayList();
+        chutesErrados = new ArrayList();
+    }
+    public string SortearPalavra()
+    {
+    }
+    public bool ChutarLetra(char chute)
+    {
+    }
+    public bool IsFimDeJogo()
+    {
+    }
+    public void ExibirStatusDoJogo()
+    {
+    }
+}

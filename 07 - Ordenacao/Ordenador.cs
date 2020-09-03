699121dbe3798a25d01f55e39dbde1887fc4bde9
Ricardo Mendes <rmcs87@gmail.com>
2020-09-03 10:06:18 -0300

    Aula de Ordenação básica

---
+++ 07 - Ordenacao/Ordenador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Ordenador
+{
+    public void Bubblesort(int[] vetor)
+    {
+        for (int j = 0; j < vetor.Length; j++)
+        {
+            for (int i = 0; i < vetor.Length - 1; i++)
+            {
+                if (vetor[i] > vetor[i + 1])
+                {
+                    int aux = vetor[i];
+                    vetor[i] = vetor[i + 1];
+                    vetor[i + 1] = aux;
+                }
+            }
+        }
+    }
+    public void PrintVetor(int[] vetor)
+    {
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.WriteLine($"V[{i}] = {vetor[i]}");
+        }
+    }
+}
+
---
+++ 07 - Ordenacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[] { 1,5,6,45,0,-1,12 };
+        Ordenador order = new Ordenador();
+        order.Bubblesort(vetor);
+        order.PrintVetor(vetor);
+    }
+}

e675bcc0a487f856a70d87f1000d44c4b1199f2b
Ricardo Mendes <rmcs87@gmail.com>
2020-09-10 10:20:55 -0300

    Adição na lista.

--- 08 - ListaOrdenada/LinkedList.cs
+++ 08 - ListaOrdenada/LinkedList.cs
@@ -3,6 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 
 
-class LinkedList
+public class LinkedList
 {
+    public Node head;
+
+	public LinkedList()
+	{
+		head = null;
+	}
+
+	/// <summary>
+	/// Add at first position.
+	/// </summary>
+	public void Add(int valor)
+	{
+		var newNode = new Node(valor);
+		newNode.next = head;
+		head = newNode;		
+	}
 }

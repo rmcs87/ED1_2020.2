36f09a41e23a0109efa94dcfbc954266b6b77c0b
Ricardo Mendes <rmcs87@gmail.com>
2020-09-10 09:59:50 -0300

    Criação do Nó.

---
+++ 08 - ListaOrdenada/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Node
+{
+    public int data;
+    public Node next;
+
+    public Node()
+    {
+        data = -1;
+        next = null;
+    }
+    public Node(int valor)
+    {
+        data = valor;
+        next = null;
+    }
+}
+

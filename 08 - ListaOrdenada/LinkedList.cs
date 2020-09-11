b473208b7efd2e3185fdb8db7290f61687c56100
Ricardo Mendes <rmcs87@gmail.com>
2020-09-11 10:13:05 -0300

    Remove

--- 08 - ListaOrdenada/LinkedList.cs
+++ 08 - ListaOrdenada/LinkedList.cs
@@ -7,39 +7,62 @@ public class LinkedList
 {
     public Node head;
 
-	public LinkedList()
-	{
-		head = null;
-	}
-	public void Add(int valor)
-	{
-		var newNode = new Node(valor);
-		newNode.next = head;
-		head = newNode;		
-	}
-	public Node Find(int valor)
-	{
-		Node aux = head;
-		while ( (aux != null) && (aux.data != valor))
-		{
-			aux = aux.next;
-		}
-		return aux;
-	}
-	public bool IsEmpty()
-	{
-		return (head == null);
-	}
-	public void Print()
-	{
-		Console.Write("[HEAD]");
-		var aux = head;
+    public LinkedList()
+    {
+        head = null;
+    }
+    public void Add(int valor)
+    {
+        var newNode = new Node(valor);
+        newNode.next = head;
+        head = newNode;
+    }
+    public Node Find(int valor)
+    {
+        Node aux = head;
+        while ((aux != null) && (aux.data != valor))
+        {
+            aux = aux.next;
+        }
+        return aux;
+    }
+    public bool IsEmpty()
+    {
+        return (head == null);
+    }
+    public void Print()
+    {
+        Console.Write("[HEAD]");
+        var aux = head;
 
-		while (aux != null)
-		{
-			Console.Write($"-> [{aux.data}]");
-			aux = aux.next;
-		}
-		Console.Write("-> [NULL]");
-	}
+        while (aux != null)
+        {
+            Console.Write($"-> [{aux.data}]");
+            aux = aux.next;
+        }
+        Console.Write("-> [NULL]");
+    }
+    public void Remove(int valor)
+    {
+        if (this.IsEmpty())
+        {
+            return;
+        }
+        Node aux = head;
+        Node ant = null;
+        while ((aux != null) && (aux.data != valor))
+        {
+            ant = aux;
+            aux = aux.next;
+        }
+        if (ant == null) //remover primeiro no
+        {
+            head = aux.next;
+        }
+        else
+        {
+            ant.next = aux.next;
+        }
+        //free by GC
+    }
 }

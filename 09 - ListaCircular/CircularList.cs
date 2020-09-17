dcfe87341a6e626e5649df21e59aaef2a1b411ce
Ricardo Mendes <rmcs87@gmail.com>
2020-09-17 10:13:26 -0300

    ListCircular: Add e Print

--- 09 - ListaCircular/CircularList.cs
+++ 09 - ListaCircular/CircularList.cs
@@ -5,8 +5,44 @@ using System.Text;
 public class CircularList
 {
     public Node head;
-	public CircularList()
-	{
-		head = null;
-	}
+    public CircularList()
+    {
+        head = null;
+    }
+    public void Add(int valor)
+    {
+        Node aux = head;
+        var newNode = new Node(valor);
+        if (head != null)
+        {
+            while (aux.next != head)
+            {
+                aux = aux.next;
+            }
+        }
+        else
+        {
+            aux = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        aux.next = head;
+    }
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.Write("Lista Vazia.");
+        }
+        Console.Write("[HEAD]");
+        var aux = head;
+
+        do
+        {
+            Console.Write($"-> [{aux.data}]");
+            aux = aux.next;
+        } while (aux != head);
+
+        Console.Write("-> [NULL]");
+    }
 }

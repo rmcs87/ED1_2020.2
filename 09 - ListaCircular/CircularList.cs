41cab9155e0119a391d6c6ad67928123ee314211
Ricardo Mendes <rmcs87@gmail.com>
2020-09-17 09:43:28 -0300

    Lista Circular: criação do projeto.

---
+++ 09 - ListaCircular/CircularList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class CircularList
+{
+    public Node head;
+	public CircularList()
+	{
+		head = null;
+	}
+}
---
+++ 09 - ListaCircular/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09___ListaCircular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

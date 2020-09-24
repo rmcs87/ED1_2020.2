8a24a5b1d710a2872de960b6c29271874d62f354
Ricardo Mendes <rmcs87@gmail.com>
2020-09-24 10:15:34 -0300

    Pilha - Methods

--- 10 - Pilha/Pilha.cs
+++ 10 - Pilha/Pilha.cs
@@ -1,8 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-public class Pilha
+﻿public class Pilha
 {
+	//Construtor
+	public Pilha()
+	{
+		//2do: Alocar a nossa pilha (TOP -> null)
+	}
+	public void Push(int valor)
+	{
+		//Inserir no topo da pilha
+	}
+	public int Pop()
+	{
+		//remove do topo e retorna o valor
+	}
+	public bool Empty()
+	{
+		//retorna se a pilha é vazia
+	}
+	public void Clear()
+	{
+		//Remove todo da Pilha
+	}
+	public int Count()
+	{
+		//retornar quantos elementos temos na pilha
+	}
 }
 

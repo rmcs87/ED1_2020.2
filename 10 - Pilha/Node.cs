f7c1fd45339bbafda3399372ec37811a84797752
Ricardo Mendes <rmcs87@gmail.com>
2020-09-24 10:06:07 -0300

    Pilha - Node

--- 10 - Pilha/Node.cs
+++ 10 - Pilha/Node.cs
@@ -1,9 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-
-public class Node
+﻿public class Node
 {
+    public int data;
+    public Node next;
+
+    public Node()
+    {
+        data = -1;
+        next = null;
+    }
+    public Node(int valor)
+    {
+        data = valor;
+        next = null;
+    }
 }
 

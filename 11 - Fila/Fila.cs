43760f2b40c615e59a66e1067299d1c0be49021c
Ricardo Mendes <rmcs87@gmail.com>
2020-10-01 10:04:45 -0300

    Fila - Declarações

--- 11 - Fila/Fila.cs
+++ 11 - Fila/Fila.cs
@@ -5,5 +5,23 @@ using System.Text;
 
 public class Fila
 {
+    public Node filaInicio;
+    public Node filaFim;
+    public int contador = 0;
+    public void Insert(Pessoa pessoa)
+    {//Insere na fila
+    }
+    public Pessoa Remove()
+    {//Remove da fila
+    }
+    public bool IsEmpty()
+    {//A Fila é vazia?
+    }
+    public void Clear()
+    {//Esvazia a fila
+    }
+    public int Count()
+    {//Retorna quantos temos na fila;
+    }
 }
 

87cb5007e7c5ee87f912048e4ab4cafafdb1aed9
Ricardo Mendes <rmcs87@gmail.com>
2020-10-01 10:18:52 -0300

    Fila - Insert

--- 11 - Fila/Fila.cs
+++ 11 - Fila/Fila.cs
@@ -8,14 +8,31 @@ public class Fila
     public Node filaInicio;
     public Node filaFim;
     public int contador = 0;
+    public Fila()
+    {
+        filaInicio = null;
+        filaFim = null;
+    }
     public void Insert(Pessoa pessoa)
-    {//Insere na fila
+    {
+        Node novo = new Node(pessoa);
+        if (IsEmpty())
+        {
+            filaInicio = novo;
+            filaFim = novo;
+        }
+        else
+        {
+            filaFim.next = novo;
+            filaFim = novo;
+        }
     }
     public Pessoa Remove()
     {//Remove da fila
     }
     public bool IsEmpty()
-    {//A Fila é vazia?
+    {
+        return filaInicio == null;
     }
     public void Clear()
     {//Esvazia a fila

6683330b5767579ff3708f5da3c9c33ecc4f787f
Ricardo Mendes <rmcs87@gmail.com>
2020-10-09 09:51:23 -0300

    Recursividade - Fatorial

---
+++ 12 - Recursividade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Recursividade");
+        Console.WriteLine(fat(5));
+    }
+
+    public static int fat(int n)
+    {
+        if(n > 1)
+        {
+            return n * fat(n-1);
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}

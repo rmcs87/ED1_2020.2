fe17f4d1aff0337ac00a38793e022a39876af3c3
Ricardo Mendes <rmcs87@gmail.com>
2020-10-09 10:21:04 -0300

    Recursividade - Fibbo

--- 12 - Recursividade/Program.cs
+++ 12 - Recursividade/Program.cs
@@ -6,7 +6,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Recursividade");
-        Console.WriteLine(fat(5));
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(fibo(i));
+        }        
+    }
+    public static int fibo(int n)
+    {
+        if (n > 1)
+        {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+        else
+        {
+            return 1;
+        }
     }
 
     public static int fat(int n)

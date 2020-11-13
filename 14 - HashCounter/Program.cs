369cc1088a5f41435333f10cc35f638554c2ad80
Ricardo Mendes <rmcs87@gmail.com>
2020-11-13 09:27:45 -0300

    Hash - Base

---
+++ 14 - HashCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14___HashCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Hash!");
+        }
+    }
+}

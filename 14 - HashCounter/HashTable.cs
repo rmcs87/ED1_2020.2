f8716cfc89b6273d868ca1bbe909978284e9c004
Ricardo Mendes <rmcs87@gmail.com>
2020-11-13 10:24:25 -0300

    Hash - Contador de Palavras Simples

--- 14 - HashCounter/HashTable.cs
+++ 14 - HashCounter/HashTable.cs
@@ -4,4 +4,51 @@ using System.Text;
 
 public class HashTable
 {
+    private int _tableSize;
+    private Cell[] table;
+    public HashTable(int tam)
+    {
+        _tableSize = tam;
+        table = new Cell[tam];
+    }
+    public void Add(string word)
+    {
+        int index = HashFunction(word);
+        while (table[index] != null && !table[index].Key.Equals(word))
+        {
+            index = (index + 1) % _tableSize;
+        }
+        if (table[index] == null)
+        {
+            table[index] = new Cell();
+            table[index].Key = word;
+        }
+        table[index].Count++;
+    }
+    public void PrintReport()
+    {
+        foreach (var cell in table)
+        {
+            if(cell != null)
+            {
+                Console.WriteLine($"Word:{cell.Key} x {cell.Count}");
+            }
+        }
+    }
+    private int HashFunction(string word)
+    {
+        var caracteres = word.ToCharArray();
+        var soma = 0;
+        foreach (var c in caracteres)
+        {
+            soma += c;
+        }
+        return soma % _tableSize;
+    }    
+}
+
+public class Cell
+{
+    public string Key { get; set; }
+    public int Count { get; set; }
 }
